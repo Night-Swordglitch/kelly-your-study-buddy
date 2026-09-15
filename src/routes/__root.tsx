045e93b0058002d9be3d94af89c739a08346094f
Night-Swordglitch <Night-Swordglitch@users.noreply.github.com>
2026-09-15 22:32:30 +0800

    Integrate new KELLY study dashboard

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -141,7 +141,7 @@ export const Route =
 
         {
           rel: "icon",
-          href: "/favicon.png",
+          href: "/favicon.png?v=3",
           type: "image/png",
         },
       ],
