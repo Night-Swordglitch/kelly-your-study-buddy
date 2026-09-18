9c31c8bdc5f97eccd65bff56c36084251812d2da
Night-Swordglitch <Night-Swordglitch@users.noreply.github.com>
2026-09-18 10:16:21 +0800

    Restore shared auth layout route files

---
+++ src/routes/_auth/route.tsx
@@ -0,0 +1,11 @@
+﻿import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { KellyIframe } from "@/components/kelly/kelly-iframe";
+
+export const Route = createFileRoute("/_auth")({
+  component: () => (
+    <>
+      <KellyIframe />
+      <Outlet />
+    </>
+  ),
+});
